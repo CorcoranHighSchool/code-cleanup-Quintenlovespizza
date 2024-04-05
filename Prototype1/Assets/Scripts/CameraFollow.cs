cd8307e4d6227077b16588220f5aafa570f5f66a
Quintenlovespizza <142622814+Quintenlovespizza@users.noreply.github.com>
2024-04-05 11:43:25 -0700

    Removed unused using statements. Removed Empty Methods. Serialized public properties.CameraFollow.cs

--- Prototype1/Assets/Scripts/CameraFollow.cs
+++ Prototype1/Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     //Vehicle game object to follow
-    public GameObject player;
+    [SerializeField] private GameObject player;
     //offset from the vehicle
     private Vector3 offset = new Vector3(0.0f, 12.0f, -10.0f);
-
     // Start is called before the first frame update
-    void Start()
+    void Start
     {
-        
-    }
 
+    }
+    
     //Update just before the frame is rendered
     void LateUpdate()
     {
