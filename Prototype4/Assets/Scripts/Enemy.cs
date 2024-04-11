0f40b0a6adfe909a1cd0f271fd5b21c77f8b8058
Quintenlovespizza <142622814+Quintenlovespizza@users.noreply.github.com>
2024-04-11 13:02:08 -0700

    removed unused libaries. serialized pubilc property. added strings to const

--- Prototype4/Assets/Scripts/Enemy.cs
+++ Prototype4/Assets/Scripts/Enemy.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 3.0f;
+    [SerializeField] private float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
+    private strings const player = player; 
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find(Player);
         enemyRb = GetComponent<Rigidbody>();
     }
 
